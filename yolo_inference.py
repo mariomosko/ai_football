ca28a46b5475891fceb351a529e304b2516370db
Mario Mosko <mario.mosko98@gmail.com>
2024-08-25 22:13:44 +0200

    second commit

---
+++ yolo_inference.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+
+
+# Download and load the model
+model = YOLO('yolov8n.pt')  # replace 'yolov8n.pt' with the desired model version
+results=model.predict('input_videos/08fd33_4.mp4',save=True)
+print(results[0])
+print('aaaaaaaaaaaaaaaaaaaaaaaaaaa')
+for box in results[0].boxes:
+    print(box)
